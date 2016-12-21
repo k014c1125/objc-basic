0672996dec6dac429e26f36c5ff50f0dfb7b4103
ITユーザー <it-user@kpc-it3151mc.kmt.neec.ac.jp>
2016-12-21 10:51:02 +0900

    @k014c1125 #6完了

--- ex2.1.2/ex2.1.2/ViewController.m
+++ ex2.1.2/ex2.1.2/ViewController.m
@@ -1,6 +1,6 @@
 //
 //  ViewController.m
-//  ex2.1.1
+//  ex2.1.2
 //
 //  Created by ITユーザー on 2016/12/21.
 //  Copyright © 2016年 ITユーザー. All rights reserved.
@@ -17,11 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    UILabel *label = [[UILabel alloc] init];
-    label.frame = CGRectMake(10, 10, 300, 50);
-    NSString *text = NSLocalizedString(@"ラベル", nil);
-    label.text=text;
-    [self.view addSubview:label];
 }
 
 - (void)didReceiveMemoryWarning {
