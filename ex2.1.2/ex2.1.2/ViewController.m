d290892a6d9bb761248b26f652369a3353f8491b
ITユーザー <it-user@kpc-it3151mc.kmt.neec.ac.jp>
2016-12-21 13:57:55 +0900

    @k014c1125 #7完了

--- ex2.1.2/ex2.1.2/ViewController.m
+++ ex2.1.2/ex2.1.2/ViewController.m
@@ -9,14 +9,19 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIImageView *image;
 @end
 
 @implementation ViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor blackColor];
+    self.image.contentMode = UIViewContentModeScaleAspectFit;
+    UIImage *img = [UIImage imageNamed:@"images"];
+    self.image.layer.cornerRadius = 30;
+    self.image.layer.masksToBounds = true;
+    self.image.image = img;
 }
 
 - (void)didReceiveMemoryWarning {
